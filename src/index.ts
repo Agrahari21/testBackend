dda573291e1f18f6f203870b9c68c7cafb859e8a
Agrahari21 <75620278+Agrahari21@users.noreply.github.com>
2024-09-23 19:17:43 +0530

    Update index.ts
    cors

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,8 @@ var corOptions = {
   ],
 };
 
-//app.use(cors({ origin: "*" }));
-app.use(cors(corOptions));
+app.use(cors({ origin: "*" }));
+//app.use(cors(corOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
