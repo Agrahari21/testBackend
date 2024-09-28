554a8da853ef85a893d4004d6776ba86208af7a8
Agrahari21 <75620278+Agrahari21@users.noreply.github.com>
2024-09-29 00:20:38 +0530

    Update dbConfig.ts

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,42 +1,14 @@
-/* import { Dialect } from "sequelize";
-
-type dialect = Dialect;
-
-const dbConfig = {
-  HOST: "localhost",
-  USER: "ubuntu",
-  PASSWORD: "Ubuntu@101",
-  DB: "food_app_db",
-  dialect: "mysql",
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
-
-export default dbConfig;
- */
 
 import { Dialect } from "sequelize";
 import { Sequelize } from "sequelize-typescript";
 
-type dLect = Dialect;
-
-//u302726401_ubuntu
-
-let db: string = process.env.DB_NAME || "fooddb1";
-//let dbType: dLect = "mariadb" || "mysql";
 
 const sequelize = new Sequelize({
-  database: db,
+  database: process.env.DB_NAME,
   dialect: "mariadb",
-  username: process.env.DB_USER || "ubuntu",
-  password: process.env.DB_PASS || "Ubuntu@101",
-  host: process.env.DB_HOST || "127.0.0.1",
-  //host: "192.168.56.1",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST, 
   port: 3306,
   models: [__dirname + "/models"],
   sync: { force: true },
